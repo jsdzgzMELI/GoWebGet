03182bffb30c714adc79bc9843e4f98e273a79b8
jsdzgzMELI <0fps4k@gmail.com>
2025-01-13 12:01:00 -0500

    Parte de buscar por ID

--- handlers/productHandler.go
+++ handlers/productHandler.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/jsdzgzMELI/GoWeb/helper"
+	"github.com/jsdzgzMELI/GoWeb/structs"
 )
 
 type Controller struct {
@@ -23,4 +25,34 @@ func ProductHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("Error loading products: %v", err)
 	}
 	json.NewEncoder(w).Encode(products)
+}
+
+func (c *Controller) GetById(w http.ResponseWriter, r *http.Request) {
+	// id := r.URL.Query().Get("id")
+	// product, err := helper.GetProductById(id, "./products.json")
+	id := chi.URLParam(r, "id")
+	product, ok := c.st[id]
+	if !ok {
+		code := http.StatusNotFound
+		body := &structs.ResponseId{Message: "product not found", Data: nil}
+		w.WriteHeader(code)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(body)
+		return
+	}
+	code := http.StatusOK
+	// AQUI TOCA ITERAR LOS PRODUCTOS Y BUSCAR EL PRODUCTO POR ID
+	body := &structs.ResponseId{Message: "product found", Data: &structs.Product{
+		ID:           id,
+		Name:         product,
+		Quantity:     0,
+		Code_value:   "",
+		Is_published: false,
+		Expiration:   "",
+		Price:        0.0,
+	}}
+
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(body)
 }
